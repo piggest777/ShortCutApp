11a03fcf79682ab8658249011fce4568d6e1bba3
Denis Rakitin <piggest@mail.ru>
2019-10-06 01:13:12 +0200

    Initial Commit

---
+++ ShortCutApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShortCutApp
+//
+//  Created by Denis Rakitin on 2019-10-06.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
